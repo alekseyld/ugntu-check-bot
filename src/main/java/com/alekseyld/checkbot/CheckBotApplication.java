33c3301de7f1d14cc979e7ce7bac6b6104ee5918
alekseyld <al.lisow1@gmail.com>
2022-11-27 23:41:01 +0300

    init commit

---
+++ src/main/java/com/alekseyld/checkbot/CheckBotApplication.java
@@ -0,0 +1,13 @@
+package com.alekseyld.checkbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alekseyld/checkbot/CheckBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekseyld.checkbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
